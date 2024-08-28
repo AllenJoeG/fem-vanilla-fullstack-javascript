b9a743fd8e465ff26a6f33851112a43fdd73a6cd
AllenJoeG <allen.joeg@gmail.com>
2024-08-28 10:36:12 -0500

    First snapshot

--- src/shared/controller.js
+++ src/shared/controller.js
@@ -1,8 +1,8 @@
-//Name could be specified if we had multiple models to control.
+// Name could be specified if we had multiple models to control.
 
 // Decoration pattern allows Intellisense to populate dynamically 
 
-//Dependency Injection design pattern
+// Dependency Injection design pattern
 
 /**
  * @typedef {import('./viewBase.js').default} View
@@ -19,12 +19,18 @@ export default class Controller {
   static init(deps) {
     const controller = new Controller(deps)
     controller.#init()
-    
+
     return controller
   }
 
-  #init( ) {
-    this.#view.render([])
+  #init() {
+    const initialData= [
+      { name: 'Joe Allen', age: 35, email: 'joe@joe.com'},
+      { name: 'Kelly', age: 42, email: 'kelly@kelly.com'},
+      { name: 'Sasha', age: 43, email: 'sasha@sasha.com'},
+    ]
+
+    this.#view.render(initialData)
   }
 
 }
--- ui/index.js
+++ ui/index.js
@@ -2,9 +2,10 @@
 
 import Controller from "../src/shared/controller.js";
 
+//Checks for window object to know if we're on web or console
 const platform = globalThis.window ? "web" : "console";
 
-//  
+// 
 const { default: View } = await import(`./../src/platforms/${platform}/view.js`)
 
 Controller.init({
