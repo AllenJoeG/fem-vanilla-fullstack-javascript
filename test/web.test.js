5a2f424096a0891e3fbb99281c93b7a236f6f28b
AllenJoeG <allen.joeg@gmail.com>
2024-08-28 13:37:05 -0500

    Test wild af

--- test/web.test.js
+++ test/web.test.js
@@ -1,6 +1,7 @@
-import { describe, it, before } from 'node:test'
+import { describe, it, before } from 'node:test';
 import Controller from '../src/shared/controller.js';
-import View from './../src/platforms/web/view.js'
+import View from './../src/platforms/web/view.js';
+import assert from 'node:assert';
 
 function getDocument(mock) {
   globalThis.alert = mock.fn()
@@ -15,8 +16,9 @@ function getDocument(mock) {
 
     querySelector: mock.fn((id) => {
       return {
+        value: 'test',
         addEventListener: mock.fn((event, fn) => {
-          // event.preventDefault = () => {}
+          
           return fn({
             preventDefault: () => {}
           })
@@ -29,11 +31,8 @@ function getDocument(mock) {
         })
       }
     }),
-    
   }
 }
-
-
 
 describe('Web app test suite', () => {
   let _controller 
@@ -44,10 +43,44 @@ describe('Web app test suite', () => {
 
   it('Given valid inputs, should update the table data', async (context) => {
     const document = getDocument(context.mock)
+    const view = new View()
+    const addRow = context.mock.method(view, view.addRow.name)
+
     _controller = Controller.init({
-      view: new View()
+      view
     })
 
-    document.querySelector.mock
+    const [
+      name,
+      age,
+      email,
+      tableBody,
+      form,
+      btnFormClear
+    ] = document.querySelector.mock.calls
+
+    assert.strictEqual(name.arguments[0], '#name')
+    assert.strictEqual(age.arguments[0], '#age')
+    assert.strictEqual(email.arguments[0], '#email')
+    assert.strictEqual(tableBody.arguments[0], '.flex-table')
+    assert.strictEqual(form.arguments[0], '#form')
+    assert.strictEqual(btnFormClear.arguments[0], '#btnFormClear')
+
+    const onSubmit = form.result.addEventListener.mock.calls[0].arguments[1]
+    const preventDefaultSpy = context.mock.fn();
+
+    assert.strictEqual(addRow.mock.callCount(), 4)
+    
+    onSubmit({
+      preventDefault: preventDefaultSpy
+    })
+
+    assert.strictEqual(addRow.mock.callCount(), 5)
+    
+    assert.deepStrictEqual(
+      addRow.mock.calls.at(4).arguments[0],
+      {name: 'test', age: 'test', email: 'test'}
+    )
+    
   })
 })
