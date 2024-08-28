da02fde6334a2b0f1b245c15843de0fcdaa00ccc
AllenJoeG <allen.joeg@gmail.com>
2024-08-28 12:20:03 -0500

    Fixed vscode setting, bunch of insane bad practice mocking haha

--- test/web.test.js
+++ test/web.test.js
@@ -1,7 +1,53 @@
-import { describe, it } from 'node:test'
+import { describe, it, before } from 'node:test'
+import Controller from '../src/shared/controller.js';
+import View from './../src/platforms/web/view.js'
+
+function getDocument(mock) {
+  globalThis.alert = mock.fn()
+
+  // Lying to JS, faking document exists
+  globalThis.document = {
+    createElement: mock.fn((name) => ({
+        classList: {
+          add: mock.fn((name) => {})
+        }
+      })),
+
+    querySelector: mock.fn((id) => {
+      return {
+        addEventListener: mock.fn((event, fn) => {
+          // event.preventDefault = () => {}
+          return fn({
+            preventDefault: () => {}
+          })
+        }),
+        reset: mock.fn(() => {}),
+        appendChild: mock.fn((child) => {
+          return {
+            
+          }
+        })
+      }
+    }),
+    
+  }
+}
+
+
 
 describe('Web app test suite', () => {
-  it('Given valid inputs, should update the table data', async () => {
-    console.log('working');
+  let _controller 
+
+  before(() => {
+    
+  })
+
+  it('Given valid inputs, should update the table data', async (context) => {
+    const document = getDocument(context.mock)
+    _controller = Controller.init({
+      view: new View()
+    })
+
+    document.querySelector.mock
   })
 })
